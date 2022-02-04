fbfd7190c28dc676e1c489ffa77a5fcf47fb008e
Burak Alp Yasar <94975878+O8pen@users.noreply.github.com>
2022-02-03 23:32:02 -0800

    110A solution

---
+++ 110A.py
@@ -0,0 +1,14 @@
+from distutils import core
+
+
+inp1 = input()
+
+correct = 'YES'
+
+for x in inp1:
+    if(x == 4 or x == 7):
+        pass
+    else:
+        correct = 'NO'
+        
+print(correct)
